03db2cdf1c1815dba756e99fe65ec5b075ca1605
Madhu <Madhu@192.168.1.11>
2021-02-02 09:17:34 +0530

    Access Specifiers

---
+++ BECA10/src/com/jspiders/pkg1/Demo.java
@@ -0,0 +1,11 @@
+package com.jspiders.pkg1;
+
+public class Demo 
+{
+  public int v1 = 10;
+  
+  public void test1()
+  {
+	System.out.println("this public-test1() of Demo class");
+  }
+}
--- BECA10/src/com/jspiders/pkg1/Run.java
+++ BECA10/src/com/jspiders/pkg1/Run.java
@@ -1,5 +1,16 @@
 package com.jspiders.pkg1;
 
-public class Run {
-
+public class Run 
+{
+    public static void main(String[] args) 
+    {
+		System.out.println("main() of Run starts...");
+		
+		Demo d1 = new Demo();
+		
+		System.out.println(d1.v1);
+		d1.test1();
+		
+		System.out.println("main() of Run ends...");
+	}
 }
