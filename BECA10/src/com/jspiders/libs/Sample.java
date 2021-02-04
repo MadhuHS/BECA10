5e463cc46f62872f6599a00da299b52e9feac81f
Madhu <Madhu@192.168.1.10>
2021-02-04 09:31:04 +0530

    Object class

---
+++ BECA10/src/com/jspiders/libs/Sample.java
@@ -0,0 +1,33 @@
+package com.jspiders.libs;
+
+public class Sample 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Program starts..");
+
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		Object obj3 = obj1;
+
+		int h1 = obj1.hashCode();
+		System.out.println(h1);
+
+		int h2 = obj2.hashCode();
+		System.out.println(h2);
+
+		int h3 = obj3.hashCode();
+		System.out.println(h3);
+
+		String s1 = obj1.toString();
+		System.out.println(s1);
+
+		boolean b1 = obj1.equals(obj2);
+		System.out.println(b1);
+
+		boolean b2 = obj1.equals(obj3);
+		System.out.println(b2);
+
+		System.out.println("Program ends..");
+	}
+}
