582f9b444980bf7ecc77ff946ac0e9d693306f90
Madhu <Madhu@192.168.1.10>
2021-02-09 08:26:13 +0530

    Generalized try-catch block

---
+++ BECA10/src/exceptionhandling/Mainclass4.java
@@ -0,0 +1,35 @@
+package exceptionhandling;
+
+public class Mainclass4
+{
+    public static void test(int n1,int n2)
+    {
+      System.out.println("test() starts...");
+      
+      System.out.println("n1 : "+n1);
+      System.out.println("n2 : "+n2);
+      
+      int[] a1 = {10,20,30,40};
+      
+      try
+      {
+        int res = n1/n2;
+        System.out.println("res : "+res);
+        System.out.println("Element at "+res+" is : "+a1[res]);
+      }
+      catch(Exception ref)
+      {
+    	System.out.println("Handled Exception");
+    	ref.printStackTrace();
+      }
+      
+      System.out.println("test() ends...");
+    }
+	public static void main(String[] args) 
+	{
+      System.out.println("main() starts...");
+      test(100,0);
+      System.out.println("main() ends...");
+	}
+
+}
