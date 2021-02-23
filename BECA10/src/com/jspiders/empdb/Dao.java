a91d0ba20263864f28a96e0c3caef0c27ce809a3
Madhu <Madhu@192.168.0.108>
2021-02-23 09:00:56 +0530

    JDBC updated

--- BECA10/src/com/jspiders/empdb/Dao.java
+++ BECA10/src/com/jspiders/empdb/Dao.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public interface Dao 
 {
    public void addEmployee(Employee emp) throws SQLException;
-   public void getEmployee(int id) throws SQLException;
-   public void updateEmployee(Employee emp) throws SQLException;
+   public Employee getEmployee(int id) throws SQLException;
+   public void updateEmployeeName(int id,String newName) throws SQLException;
    public void deleteEmployee(int id) throws SQLException;
 }
--- BECA10/src/com/jspiders/empdb/MySqlimpl.java
+++ BECA10/src/com/jspiders/empdb/MySqlimpl.java
@@ -2,11 +2,13 @@ package com.jspiders.empdb;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MySqlimpl implements Dao
 {
 	Connection con;
+	
 	@Override
 	public void addEmployee(Employee emp) throws SQLException 
 	{
@@ -15,22 +17,55 @@ public class MySqlimpl implements Dao
 		PreparedStatement pm = con.prepareStatement(insert);
 		pm.setInt(1,emp.getId());
 		pm.setString(2,emp.getName());
+		pm.setString(3,emp.getJob());
+		pm.setString(4,emp.getHiredate());
+		pm.setDouble(5,emp.getSal());
+		pm.setDouble(6,emp.getComm());
+		pm.setInt(7,emp.getDeptno());
 		
-		pm.execute();
+		pm.execute();//save data
 	}
 	@Override
-	public void getEmployee(int id) throws SQLException {
-		// TODO Auto-generated method stub
+	public Employee getEmployee(int id) throws SQLException 
+	{
+		String selectEmployee = "SELECT * from emp where empno = ? ";
+		PreparedStatement pm = con.prepareStatement(selectEmployee);
+		pm.setInt(1, id);
+		ResultSet rs = pm.executeQuery();
 		
+		int eid = rs.getInt("empno");
+		String name = rs.getString("empname");
+		String job = rs.getString("job");
+		String hiredate = rs.getString("hiredate");
+		double sal = rs.getDouble("sal");
+		double comm = rs.getDouble("comm");
+		int deptno = rs.getInt("deptno");
+		
+		Employee emp = new Employee(eid, name, job, sal, comm, hiredate, deptno);
+		
+       return emp;		
 	}
 	@Override
-	public void updateEmployee(Employee emp) throws SQLException {
-		// TODO Auto-generated method stub
+	public void updateEmployeeName(int id,String newName) throws SQLException {
 		
+		String updateName = "UPDATE emp.ename SET name = ?  where empno = ?";
+		PreparedStatement pm = con.prepareStatement(updateName);
+		pm.setString(1, newName);
+		pm.setInt(2,id);
+		int count = pm.executeUpdate();
+		System.out.println(count+" rows updated");
 	}
 	@Override
-	public void deleteEmployee(int id) throws SQLException {
-		// TODO Auto-generated method stub
-		
+	public void deleteEmployee(int id) throws SQLException 
+	{
+		String deleteEmp = "DELETE from emp.ename where empno = ?";
+		PreparedStatement pm = con.prepareStatement(deleteEmp);
+		pm.setInt(1, id);
+		int count = pm.executeUpdate();
+		System.out.println(count+" rows delete");
 	}
 }
+
+
+
+
